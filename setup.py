3ae7592ac7d3ed9584b9fdf46bf328c1b42cc1ec
Ivan <vanzaj@gmail.com>
2013-09-25 19:49:36 +0800

    distribute setup

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+from dacets import (__title__, __version__, __author__, __license__)
+
+setup(
+    name=__title__,
+    version=__version__,
+    author=__author__,
+    author_email='vanzaj@gmail.com',
+    packages=['dacets', 'dacets.tests'],
+    scripts=['bin/dct'],
+    url='http://dacets.net/',
+    license=__license__,
+    description='facets of (research) datasets',
+    long_description=open('README.md').read(),
+    keywords = ['scientific data management', 'electronic data capture'],
+    classifiers = [
+            'Development Status :: 3 - Alpha',
+            'Environment :: Console',
+            'Intended Audience :: Science/Research',
+            'Topic :: Science/Research :: Data Management',
+            'License :: OSI Approved :: BSD License',
+            'Programming Language :: Python :: 2.7',
+        ],
+    install_requires=[
+        'Sphinx >= 1.1.3',
+        'PyYAML >= 3.10',
+    ],
+)
